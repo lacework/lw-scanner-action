c5202862e159659d7aad1dc532f5fa4e3fbb3044
Tim Arenz <tim@arenz.cc>
2021-08-17 10:18:05 +0200

    New if again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,22 +8,22 @@ if [ $? != 0 ]; then
 fi
 
 # Cecking results
-if [[ ${INPUT_FAIL_ONLY_IF_VULNERABILITIES_FIXABLE} && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.fixable_vulnerabilities') -ge 1 ]]; then
+if [[ ${INPUT_FAIL_ONLY_IF_VULNERABILITIES_FIXABLE} -eq "true" && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.fixable_vulnerabilities') -ge 1 ]]; then
     echo "At least one fixable vulnerabilty found."
     exit 1
-elif [[ ${INPUT_FAIL_IF_CRITICAL_VULNERABILITIES_FOUND} && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.critical_vulnerabilities') -ge 1 ]]; then
+elif [[ ${INPUT_FAIL_IF_CRITICAL_VULNERABILITIES_FOUND} -eq "true" && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.critical_vulnerabilities') -ge 1 ]]; then
     echo "At least one critical vulnerabilty found."
     exit 1
-elif [[ ${INPUT_FAIL_IF_HIGH_VULNERABILITIES_FOUND} && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.high_vulnerabilities') -ge 1 ]]; then
+elif [[ ${INPUT_FAIL_IF_HIGH_VULNERABILITIES_FOUND} -eq "true" && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.high_vulnerabilities') -ge 1 ]]; then
     echo "At least one high vulnerabilty found."
     exit 1
-elif [[ ${INPUT_FAIL_IF_MEDIUM_VULNERABILITIES_FOUND} && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.medium_vulnerabilities') -ge 1 ]]; then
+elif [[ ${INPUT_FAIL_IF_MEDIUM_VULNERABILITIES_FOUND} -eq "true" && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.medium_vulnerabilities') -ge 1 ]]; then
     echo "At least one medium vulnerabilty found."
     exit 1
-elif [[ ${INPUT_FAIL_IF_LOW_VULNERABILITIES_FOUND} && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.low_vulnerabilities') -ge 1 ]]; then
+elif [[ ${INPUT_FAIL_IF_LOW_VULNERABILITIES_FOUND} -eq "true" && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.low_vulnerabilities') -ge 1 ]]; then
     echo "At least one low vulnerabilty found."
     exit 1
-elif [[ ${INPUT_FAIL_IF_INFO_VULNERABILITIES_FOUND} && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.info_vulnerabilities') -ge 1 ]]; then
+elif [[ ${INPUT_FAIL_IF_INFO_VULNERABILITIES_FOUND} -eq "true" && $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.info_vulnerabilities') -ge 1 ]]; then
     echo "At least one info vulnerabilty found."
     exit 1
 else
