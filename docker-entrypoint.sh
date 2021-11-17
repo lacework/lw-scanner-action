d3940b75dc8adfae460d01c734d5e8ff53b2e102
Tim Arenz <tim@arenz.cc>
2021-11-17 17:59:20 +0100

    Add exit codes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ export LW_SCANNER_DISABLE_UPDATES=${LW_SCANNER_DISABLE_UPDATES:-true}
 # Add parameters based on arguments
 export SCANNER_PARAMETERS=""
 if [ ${INPUT_USE_POLICY} = "true" ]; then
-    export SCANNER_PARAMETERS="${SCANNER_PARAMETERS} --policy"
+    export SCANNER_PARAMETERS="${SCANNER_PARAMETERS} --policy --fail-on-violation-exit-code 11 --critical-violation-exit-code 12 --high-violation-exit-code 13 --medium-violation-exit-code 14 --low-violation-exit-code 15 --info-violation-exit-code 16"
 fi
 if [ ${INPUT_SAVE_HTML_REPORT} = "true" ]; then
     export SCANNER_PARAMETERS="${SCANNER_PARAMETERS} --html"
@@ -17,38 +17,33 @@ fi
 
 # Run scanner
 /usr/local/bin/lw-scanner image evaluate ${INPUT_IMAGE_NAME} ${INPUT_IMAGE_TAG} --build-plan ${GITHUB_REPOSITORY} --build-id ${GITHUB_RUN_ID} --data-directory ${GITHUB_WORKSPACE} ${SCANNER_PARAMETERS}
+export LW_SCANNER_EXIT_CODE=$?
 
-# Skip custom fail actions if Lacework policies are used
+# Exit if check is failed
+if [ ${INPUT_USE_POLICY} = "false" ] && [ ${LW_SCANNER_EXIT_CODE} != 0 ]; then
+    echo "Vulnerability scan failed. Failing action as security can not be guaranteed."
+    exit 1
+fi
 
-if [ ${INPUT_USE_POLICY} = "true" ]; then
-
-    # Exit if check is failed
-    if [ $? != 0 ]; then
-        echo "Vulnerability scan failed. Failing action as security can not be guaranteed."
-        exit 1
-    fi
-
-    # Checking results
-    if [ ${INPUT_FAIL_ONLY_IF_VULNERABILITIES_FIXABLE} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.fixable_vulnerabilities') -ge 1 ]; then
-        echo "At least one fixable vulnerabilty found."
-        exit 1
-    elif [ ${INPUT_FAIL_IF_CRITICAL_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.critical_vulnerabilities') -ge 1 ]; then
-        echo "At least one critical vulnerabilty found."
-        exit 1
-    elif [ ${INPUT_FAIL_IF_HIGH_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.high_vulnerabilities') -ge 1 ]; then
-        echo "At least one high vulnerabilty found."
-        exit 1
-    elif [ ${INPUT_FAIL_IF_MEDIUM_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.medium_vulnerabilities') -ge 1 ]; then
-        echo "At least one medium vulnerabilty found."
-        exit 1
-    elif [ ${INPUT_FAIL_IF_LOW_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.low_vulnerabilities') -ge 1 ]; then
-        echo "At least one low vulnerabilty found."
-        exit 1
-    elif [ ${INPUT_FAIL_IF_INFO_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.info_vulnerabilities') -ge 1 ]; then
-        echo "At least one info vulnerabilty found."
-        exit 1
-    else
-        exit 0
-    fi
-
+# Checking results
+if [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_ONLY_IF_VULNERABILITIES_FIXABLE} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.fixable_vulnerabilities') -ge 1 ]; then
+    echo "At least one fixable vulnerabilty found."
+    exit 1
+elif [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_IF_CRITICAL_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.critical_vulnerabilities') -ge 1 ]; then
+    echo "At least one critical vulnerabilty found."
+    exit 1
+elif [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_IF_HIGH_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.high_vulnerabilities') -ge 1 ]; then
+    echo "At least one high vulnerabilty found."
+    exit 1
+elif [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_IF_MEDIUM_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.medium_vulnerabilities') -ge 1 ]; then
+    echo "At least one medium vulnerabilty found."
+    exit 1
+elif [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_IF_LOW_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.low_vulnerabilities') -ge 1 ]; then
+    echo "At least one low vulnerabilty found."
+    exit 1
+elif [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_IF_INFO_VULNERABILITIES_FOUND} = "true" ] && [ $(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.info_vulnerabilities') -ge 1 ]; then
+    echo "At least one info vulnerabilty found."
+    exit 1
+else
+    exit ${LW_SCANNER_EXIT_CODE}
 fi
