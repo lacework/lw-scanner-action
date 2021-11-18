91850bf57585b2824b5ab446157548e597e9ac3c
Tim Arenz <tim@arenz.cc>
2021-11-18 09:32:46 +0100

    Minor fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -44,35 +44,35 @@ if [ ${INPUT_USE_POLICY} = "false" ] && [ ${INPUT_FAIL_BUILD} = "true" ]; then
 	critical)
         CRITICAL_VULNS_FOUND=$(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.cve.critical_vulnerabilities')
         if [ ${CRITICAL_VULNS_FOUND} -ge 1 ]; then
-            echo "${CRITICAL_VULNS_FOUND} CRITICAL vulnerabilities found. Exiting with code 1"
+            echo "${CRITICAL_VULNS_FOUND} critical vulnerabilities found. Exiting with code 1"
             exit 1
         fi
 		;;
 	high)
         HIGH_VULNS_FOUND=$(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.cve.high_vulnerabilities')
         if [  ${HIGH_VULNS_FOUND} -ge 1 ]; then
-            echo "${HIGH_VULNS_FOUND} HIGH vulnerabilities found. Exiting with code 1"
+            echo "${HIGH_VULNS_FOUND} high vulnerabilities found. Exiting with code 1"
             exit 1
         fi
 		;;
 	medium)
         MEDIUM_VULNS_FOUND=$(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.cve.medium_vulnerabilities')
         if [  ${MEDIUM_VULNS_FOUND} -ge 1 ]; then
-            echo "${MEDIUM_VULNS_FOUND} MEDIUM vulnerabilities found. Exiting with code 1"
+            echo "${MEDIUM_VULNS_FOUND} medium vulnerabilities found. Exiting with code 1"
             exit 1
         fi
 		;;
 	low)
         LOW_VULNS_FOUND=$(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.cve.low_vulnerabilities')
         if [  ${LOW_VULNS_FOUND} -ge 1 ]; then
-            echo "${LOW_VULNS_FOUND} LOW vulnerabilities found. Exiting with code 1"
+            echo "${LOW_VULNS_FOUND} low vulnerabilities found. Exiting with code 1"
             exit 1
         fi
 		;;
 	info)
         INFO_VULNS_FOUND=$(cat ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json | jq '.cve.INFO_vulnerabilities')
         if [  ${INFO_VULNS_FOUND} -ge 1 ]; then
-            echo "${INFO_VULNS_FOUND} INFO vulnerabilities found. Exiting with code 1"
+            echo "${INFO_VULNS_FOUND} info vulnerabilities found. Exiting with code 1"
             exit 1
         fi
 		;;
