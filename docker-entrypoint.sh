439c2c55020f12a6fe43a7ba1fb78c798da39f66
Tim Arenz <tim@arenz.cc>
2023-09-21 15:28:19 +0200

    fix: output for non job-summary runs (#60)
    * chore(refactor): fix console and summary output

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -o pipefail
 
+# Set Lacework credentials as inline scanner environment variable
 export LW_ACCOUNT_NAME=${INPUT_LW_ACCOUNT_NAME}
 export LW_ACCESS_TOKEN=${INPUT_LW_ACCESS_TOKEN}
 
@@ -39,13 +41,12 @@ fi
 # Remove old scanner evaluation, if cached somehow
 rm ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json &>/dev/null || true
 
-# Run scanner
 /opt/lacework/lw-scanner image evaluate ${INPUT_IMAGE_NAME} ${INPUT_IMAGE_TAG} \
-  --build-plan ${GITHUB_REPOSITORY} \
-  --build-id ${GITHUB_RUN_ID} \
-  --data-directory ${GITHUB_WORKSPACE} \
-  --policy \
-  --fail-on-violation-exit-code 1 ${SCANNER_PARAMETERS} 1> results.stdout
+    --build-plan ${GITHUB_REPOSITORY} \
+    --build-id ${GITHUB_RUN_ID} \
+    --data-directory ${GITHUB_WORKSPACE} \
+    --policy \
+    --fail-on-violation-exit-code 1 ${SCANNER_PARAMETERS} | tee results.stdout
 
 export SCANNER_EXIT_CODE=$?
 
