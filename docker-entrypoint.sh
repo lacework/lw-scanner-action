afaac25afcb911cf8f63161bc2f2a39b701c14d7
Lei Jin <lei.jin@lacework.net>
2024-10-02 21:04:05 +0000

    Fix: Fix the image location for local scanner
    Signed-off-by: Lei Jin <lei.jin@lacework.net>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -41,7 +41,7 @@ fi
 # Remove old scanner evaluation, if cached somehow
 rm ${GITHUB_WORKSPACE}/evaluations/${INPUT_IMAGE_NAME}/${INPUT_IMAGE_TAG}/evaluation_*.json &>/dev/null || true
 
-/opt/lacework/lw-scanner image evaluate ${INPUT_IMAGE_NAME} ${INPUT_IMAGE_TAG} \
+/app/vulnerability/scanner/lacework/local-scanner/main/local-scanner.binary image evaluate ${INPUT_IMAGE_NAME} ${INPUT_IMAGE_TAG} \
     --build-plan ${GITHUB_REPOSITORY} \
     --build-id ${GITHUB_RUN_ID} \
     --data-directory ${GITHUB_WORKSPACE} \
